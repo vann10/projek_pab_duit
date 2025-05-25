6857e2ca0041712901d5a329b0b8869fdddec60c
naufalfzml <naufalfzml07@gmail.com>
2025-05-25 11:05:22 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,26 +9,21 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dukun Cuaca',
+      title: 'DOMPET\'Q',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       initialRoute: '/',
-<<<<<<< HEAD
-<<<<<<< HEAD
-      routes: {'/': (context) => const HomePage()},
-=======
-      routes: {'/': (context) => const Login()},
->>>>>>> parent of 5b28002 (Merge branch 'main' of https://github.com/vann10/projek_pab_duit)
-=======
-      routes: {'/': (context) => const Login()},
->>>>>>> parent of 5b28002 (Merge branch 'main' of https://github.com/vann10/projek_pab_duit)
+      routes: {
+        '/': (context) => LoadingPage(),
+        '/home': (context) => const HomePage(),
+      },
     );
   }
 }
